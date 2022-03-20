acbedb701456c5a3f8458e5d875eb4d92781cf49
Aleksandr Streltsov <jness.pro@ya.ru>
2022-03-20 23:06:11 +0300

    я тоже ненавижу преступность

---
+++ calculator_01/src/main/java/Calculator.java
@@ -0,0 +1,117 @@
+public class Calculator {
+
+    public void calc(String userInput) {
+        int operatorCounter = 0;
+        for (int i = 0; i < userInput.length(); i++) {
+            char nextChar = userInput.charAt(i);
+            if (nextChar == '+' || nextChar == '-' || nextChar == '/' || nextChar == '*') {
+                operatorCounter++;
+            }
+        }
+
+        if (operatorCounter != 1) {
+            throw new IllegalArgumentException("Несколько операторов не поддерживаются");
+        }
+
+        char operator = '?';
+        int operatorIndex;
+        operatorIndex = userInput.indexOf('+');
+        if (operatorIndex != -1) {
+            operator = '+';
+        }
+
+        if (operator == '?') {
+            operatorIndex = userInput.indexOf('-');
+            if (operatorIndex != -1) {
+                operator = '-';
+            }
+        }
+
+        if (operator == '?') {
+            operatorIndex = userInput.indexOf('/');
+            if (operatorIndex != -1) {
+                operator = '/';
+            }
+        }
+
+        if (operator == '?') {
+            operatorIndex = userInput.indexOf('*');
+            if (operatorIndex != -1) {
+                operator = '*';
+                operatorCounter++;
+            }
+        }
+
+        if (operatorCounter != 1) {
+            throw new IllegalArgumentException("Криво. Пример: 6+1");
+        }
+
+        try {
+            String firstNumber = userInput.substring(0, operatorIndex);
+            String secondNumber = userInput.substring(operatorIndex + 1);
+
+            int firstNum = parseNumber(firstNumber);
+            int secondNum = parseNumber(secondNumber);
+
+            switch (operator) {
+                case '+':
+                    System.out.println("Input: " + userInput + ". Result: " + (firstNum + secondNum));
+                    break;
+                case '-':
+                    System.out.println("Input: " + userInput + ". Result: " + (firstNum - secondNum));
+                    break;
+                case '/':
+                    System.out.println("Input: " + userInput + ". Result: " + (firstNum / secondNum));
+                    break;
+                case '*':
+                    System.out.println("Input: " + userInput + ". Result: " + (firstNum * secondNum));
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + operator);
+            }
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Что-то не так с цифорками: " + e.getMessage());
+        }
+    }
+
+    private int parseNumber(String numberAsString) {
+        int num;
+        try {
+            num = Integer.parseInt(numberAsString);
+            if (num < 1 || num > 10) {
+                throw new IllegalArgumentException("Нельзя числа меньше 1 и больше 10");
+            }
+        } catch (NumberFormatException e) {
+            num = toArabic(numberAsString);
+        }
+
+        return num;
+    }
+
+    private int toArabic(String rome) {
+        switch (rome) {
+            case "I":
+                return 1;
+            case "II":
+                return 2;
+            case "III":
+                return 3;
+            case "IV":
+                return 4;
+            case "V":
+                return 5;
+            case "VI":
+                return 6;
+            case "VII":
+                return 7;
+            case "VIII":
+                return 8;
+            case "IX":
+                return 9;
+            case "X":
+                return 10;
+            default:
+                throw new IllegalArgumentException("Значение [" + rome + "] не допустимо");
+        }
+    }
+}
