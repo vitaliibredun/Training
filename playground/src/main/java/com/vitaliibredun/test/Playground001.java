6d3c1da22f3a84787116974365bf8367d6f9566a
Aleksandr Streltsov <jness.pro@ya.ru>
2022-03-22 22:13:51 +0300

    я тоже ненавижу преступность

--- playground/src/main/java/com/vitaliibredun/test/Playground001.java
+++ playground/src/main/java/com/vitaliibredun/test/Playground001.java
@@ -1,7 +1,29 @@
 package com.vitaliibredun.test;
 
+import org.apache.commons.lang3.StringUtils;
+
+
 public class Playground001 {
     public static void main(String[] args) {
-        
+        validate(args);
+        String name = trimAndCapitalize(args[0]);
+        sayHi(name);
+    }
+
+    private static void validate(String[] args) {
+        if (args.length == 0)
+            throw new IllegalStateException("Мне нужно имя");
+        if (StringUtils.isBlank(args[0])) {
+            throw new IllegalArgumentException("Имя пустое");
+        }
+    }
+
+    static void sayHi(String name) {
+        System.out.println("Hey, " + name);
+    }
+
+    static String trimAndCapitalize(String name) {
+        String trim = StringUtils.trim(name);
+        return StringUtils.capitalize(trim);
     }
 }
